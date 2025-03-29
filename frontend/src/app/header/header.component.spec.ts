706a969e1a09841f4febd407a50e6fbe437375d0
Sithum Raigamage <sraig2002@gmail.com>
2025-03-29 19:01:55 +0530

    test

--- frontend/src/app/header/header.component.spec.ts
+++ frontend/src/app/header/header.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from './header.component';
+import { ActivatedRoute } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { ThemeToggleButtonComponent } from './theme-toggle-button/theme-toggle-button.component';
+import { NotificationDropdownComponent } from './notification-dropdown/notification-dropdown.component';
+import { UserDropdownComponent } from './user-dropdown/user-dropdown.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,9 +12,14 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HeaderComponent],
+      imports: [
+        RouterModule,
+        ThemeToggleButtonComponent,
+        NotificationDropdownComponent,
+        UserDropdownComponent
+      ],
       providers: [
-        { provide: ActivatedRoute, useValue: {} } // Provide a mock ActivatedRoute
+        { provide: ActivatedRoute, useValue: {} }
       ]
     }).compileComponents();
 
--- frontend/src/app/header/user-dropdown/dropdown.directive.spec.ts
+++ frontend/src/app/header/user-dropdown/dropdown.directive.spec.ts
@@ -1,10 +1,25 @@
 import { ElementRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { DropdownDirective } from '../../directives/dropdown.directive';
 
 describe('DropdownDirective', () => {
+  let directive: DropdownDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: ElementRef,
+          useValue: { nativeElement: document.createElement('div') }
+        }
+      ]
+    });
+
+    const elementRef = TestBed.inject(ElementRef);
+    directive = new DropdownDirective(elementRef);
+  });
+
   it('should create an instance', () => {
-    const elementRef = new ElementRef(document.createElement('div'));
-    const directive = new DropdownDirective(elementRef);
     expect(directive).toBeTruthy();
   });
 });
